ea33f15bddfdb2aaf345c6d33efb0d4a9fcb08e3
Matt Cox <mattcoxpdx@gmail.com>
2014-07-04 19:30:29 -0700

    Serves a view

--- start.go
+++ start.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"io/ioutil"
+	"html/template"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, internet world")
+type Post struct {
+    Title string
+    Body  []byte
+}
+
+func loadPost(title string) (*Post, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+	    return nil, err
+    }
+    return &Post{Title: title, Body: body}, nil
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/post/"):]
+        p, err := loadPost(title)
+	if err != nil {
+		p = &Post{Title: title}
+	}
+	t, _ := template.ParseFiles("app/views/post.html")
+	t.Execute(w, p)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/post/", postHandler)
 	http.ListenAndServe(":8080", nil)
 }
